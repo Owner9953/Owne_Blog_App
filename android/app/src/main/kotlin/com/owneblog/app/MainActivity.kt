394afa64ee09c1c941a90d2d1400c99810c14738
Owner9953 <jhon.deo.yt@gmail.com>
2023-12-18 14:06:39 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/owneblog/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.owneblog.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app_export.dart
@@ -0,0 +1,9 @@
+export 'package:owne_blog/core/constants/constants.dart';
+export 'package:owne_blog/core/utils/color_constant.dart';
+export 'package:owne_blog/core/utils/image_constant.dart';
+export 'package:owne_blog/core/utils/size_utils.dart';
+export 'package:owne_blog/routes/app_routes.dart';
+export 'package:owne_blog/theme/app_decoration.dart';
+export 'package:owne_blog/theme/app_style.dart';
+export 'package:owne_blog/widgets/custom_image_view.dart';
+export 'package:owne_blog/core/utils/date_time_utils.dart';
